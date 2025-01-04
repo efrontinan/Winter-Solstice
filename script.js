38509877813bbbfa7143fa464c719adbd6c23e8b
efrontinan <elenauxui@gmail.com>
2025-01-04 19:52:56 +0100

    Hero animations done

--- script.js
+++ script.js
@@ -8,10 +8,23 @@ moon.id = 'moon'
 
 newDiv.appendChild(moon)
 
-const sun = document.createElement('div')
-sun.id = 'sun'
-
-newDiv.appendChild(sun)
-
-
 header.insertBefore(newDiv, header.children[0])
+
+const h1 = document.querySelector('h1')
+
+const h2 = document.createElement('h2')
+h2.textContent = 'The shortest night'
+console.log(h2)
+
+header.insertBefore(h2, header.children[2])
+
+const stars = document.createElement('div')
+stars.classList = 'stars'
+
+// const twinkling = document.createElement('div')
+// twinkling.classList = 'twinkling'
+
+// header.insertBefore(twinkling, header.children[0])
+header.insertBefore(stars, header.children[0])
+
+console.log(header.children)
